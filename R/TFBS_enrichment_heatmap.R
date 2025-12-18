ab99e180324eeaaea927bffcbd5c3f2f8918b321
Qingjie-Yu <yuqingjie1112@gmail.com>
2025-12-18 15:52:59 -0500

    Refine legend

--- R/TFBS_enrichment_heatmap.R
+++ R/TFBS_enrichment_heatmap.R
@@ -53,8 +53,8 @@ draw_heatmap <- function(data, out_path, col_fun, name, apply_cluster = FALSE, a
   )
   
   pdf(out_path, width = pdf_width, height = pdf_height)
-  draw(h, heatmap_legend_side = "right", padding = unit(c(4, 4, 4, 4), "mm"))
-  draw(lgd,  x = unit(1, "npc") - unit(30, "mm"), y = unit(0.5, "npc"), just = c("left", "center"))
+  draw(h, padding = unit(c(4, 4, 4, 20), "mm"))
+  draw(lgd,  x = unit(1, "npc") - unit(18, "mm"), y = unit(0.5, "npc"), just = c("left", "center"))
   dev.off()  
   cat(glue("Saved heatmap: {out_path}"), "\n")
 }
