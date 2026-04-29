96398db6512945955a4a2e3aa452684bcb3310c3
Qingjie-Yu <yuqingjie1112@gmail.com>
2026-04-29 16:44:28 -0400

    debug

--- R/differential_analysis.R
+++ R/differential_analysis.R
@@ -1,4 +1,4 @@
-differential_analysis <- function(cm_path, conditions, sample_names = NULL, out_dir = "./", col_cluster_file_path = NULL, ref_genome = "hg38", min_support = 2, lfc_threshold = 0.5, p_threshold = 0.05, p_type = c("fdr", "nominal", "bonferroni"), apply_annotation = TRUE, plot = TRUE) {
+differential_analysis <- function(cm_path, conditions, sample_names = NULL, out_dir = "./", col_cluster_file_path = NULL, ref_genome = "hg38", min_support = 2, lfc_threshold = 0.5, p_threshold = 0.05, p_type = c("fdr", "nominal", "bonferroni"), norm_method = c("tmm", "libsize"), apply_annotation = TRUE, plot = TRUE) {
   if (!dir.exists(out_dir)) {
     dir.create(out_dir, recursive = TRUE)
   }
@@ -8,7 +8,7 @@ differential_analysis <- function(cm_path, conditions, sample_names = NULL, out_
   cat("  Differential Regions")
   cat("\n", strrep("=", 40), "\n", sep = "")
 
-  dr_result <- differential_regions(cm_path = cm_path, conditions = conditions, sample_names = sample_names, out_dir = out_dir, col_cluster_file_path = col_cluster_file_path, min_support = min_support, lfc_threshold = lfc_threshold, p_threshold = p_threshold, p_type = p_type)
+  dr_result <- differential_regions(cm_path = cm_path, conditions = conditions, sample_names = sample_names, out_dir = out_dir, col_cluster_file_path = col_cluster_file_path, min_support = min_support, lfc_threshold = lfc_threshold, p_threshold = p_threshold, p_type = p_type, norm_method = norm_method)
 
   # Step2: generate differential heatmap
   if (plot) {
